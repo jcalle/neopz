616f85a3f2da0bf08a7a316f4648193cc24c69f1
Francisco Orlandini <francisco.orlandini@gmail.com>
2019-02-08 17:06:05 -0200

    [acoustics] first test with concentric mesh after refactor

--- Projects/AcousticsFourierAnalysis/AcousticsFourierAnalysis.cpp
+++ Projects/AcousticsFourierAnalysis/AcousticsFourierAnalysis.cpp
@@ -1,27 +1,33 @@
 #include "TPZAcousticsSimulation.h"
 
-enum ESimulationCases{ freqAxiSymmetric, freqHomogeneous2D, freqHeterogeneous2D,
-                      timeAxiSymmetric, timeHomogeneous2D, timeHeterogeneous2D};
+enum ESimulationCases{ freqAxiSymmetric, freqHomogeneous2D, freqConcentric2D,
+                      timeAxiSymmetric, timeHomogeneous2D, timeConcentric2D};
+
+void ConfigureConcentricCase(TPZAcousticsSimulation &sim);
+void ConfigureFreqConcentricCase(TPZAcousticsSimulation &sim);
 
 void ConfigureHomogeneousCase(TPZAcousticsSimulation &sim);
 void ConfigureFreqHomogeneousCase(TPZAcousticsSimulation &sim);
 
 int main(int argc, char *argv[]) {
     TPZAcousticsSimulation sim;
-    ESimulationCases simCase = ESimulationCases::freqHomogeneous2D;
+//    ESimulationCases simCase = ESimulationCases::freqHomogeneous2D;
+    ESimulationCases simCase = ESimulationCases::freqConcentric2D;
     switch(simCase){
         case ESimulationCases::freqAxiSymmetric:
             break;
         case ESimulationCases::freqHomogeneous2D:
             ConfigureFreqHomogeneousCase(sim);
             break;
-        case ESimulationCases::freqHeterogeneous2D:
+        case ESimulationCases::freqConcentric2D:
+            ConfigureFreqConcentricCase(sim);
             break;
         case ESimulationCases::timeAxiSymmetric:
             break;
         case ESimulationCases::timeHomogeneous2D:
             break;
-        case ESimulationCases::timeHeterogeneous2D:
+        case ESimulationCases::timeConcentric2D:
+            ConfigureConcentricCase(sim);
             break;
         default:
             PZError<<"What are you trying to run?"<<std::endl;
@@ -31,8 +37,48 @@ int main(int argc, char *argv[]) {
 }
 
 
-void ConfigureHomogeneousCase(TPZAcousticsSimulation &sim){
+void ConfigureConcentricCase(TPZAcousticsSimulation &sim){
 
+    sim.fSimData.fSourceSettings.posX = 0;
+    sim.fSimData.fSourceSettings.posY = 0;
+    sim.fSimData.fSourceSettings.amplitude = 1;
+    sim.fSimData.fSourceSettings.centralFrequency = 2*M_PI*18000;
+    sim.fSimData.fSourceSettings.peakTime = 1./(18000);
+
+    sim.fSimData.fSimulationSettings.simType = SPZAcousticData::ESimulationType::timeDomain;
+    sim.fSimData.fSimulationSettings.meshName = "realWEll.geo";
+    sim.fSimData.fSimulationSettings.boundType = SPZAcousticData::EBoundType::softwall;
+    sim.fSimData.fSimulationSettings.nElemPerLambda = 12;
+    sim.fSimData.fSimulationSettings.totalTime = sim.fSimData.fSourceSettings.peakTime * 12;
+    sim.fSimData.fSimulationSettings.cfl = 0.2;
+    sim.fSimData.fSimulationSettings.nTimeSteps = 600;
+    sim.fSimData.fSimulationSettings.isCflBound = true;
+    sim.fSimData.fSimulationSettings.pOrder = 2;
+    sim.fSimData.fSimulationSettings.filterBoundaryEqs = true;
+    sim.fSimData.fSimulationSettings.axiSymmetricSimulation = false;
+    sim.fSimData.fSimulationSettings.nThreads = 8;
+
+
+    sim.fSimData.fOutputSettings.resultsDir = "results/concentricTime/";
+    sim.fSimData.fOutputSettings.printGmeshVtk = true;
+    sim.fSimData.fOutputSettings.printGmeshTxt = true;
+    sim.fSimData.fOutputSettings.printCmeshTxt = true;
+    sim.fSimData.fOutputSettings.printCmeshVtk = true;
+    sim.fSimData.fOutputSettings.vtkSol = true;
+    sim.fSimData.fOutputSettings.vtkResolution = 0;
+
+}
+
+void ConfigureFreqConcentricCase(TPZAcousticsSimulation &sim){
+    ConfigureConcentricCase(sim);
+    sim.fSimData.fSimulationSettings.simType = SPZAcousticData::ESimulationType::frequencyDomain;
+    sim.fSimData.fOutputSettings.resultsDir = "results/concentricFreq/";
+    sim.fSimData.fFourierSettings.nSamples = 150;
+    sim.fSimData.fFourierSettings.wMax = sim.fSimData.fSourceSettings.centralFrequency * 3;
+    sim.fSimData.fFourierSettings.alphaFreqShift = -1;
+}
+
+void ConfigureHomogeneousCase(TPZAcousticsSimulation &sim){
     sim.fSimData.fSimulationSettings.simType = SPZAcousticData::ESimulationType::timeDomain;
     sim.fSimData.fSimulationSettings.meshName = "wellMesh.geo";
     sim.fSimData.fSimulationSettings.boundType = SPZAcousticData::EBoundType::softwall;
@@ -54,7 +100,7 @@ void ConfigureHomogeneousCase(TPZAcousticsSimulation &sim){
     sim.fSimData.fSourceSettings.centralFrequency = 2*M_PI*100;
 
 
-    sim.fSimData.fOutputSettings.resultsDir = "results/";
+    sim.fSimData.fOutputSettings.resultsDir = "results/homogeneousTime/";
     sim.fSimData.fOutputSettings.printGmeshVtk = true;
     sim.fSimData.fOutputSettings.printGmeshTxt = true;
     sim.fSimData.fOutputSettings.printCmeshTxt = true;
@@ -67,7 +113,8 @@ void ConfigureHomogeneousCase(TPZAcousticsSimulation &sim){
 void ConfigureFreqHomogeneousCase(TPZAcousticsSimulation &sim){
     ConfigureHomogeneousCase(sim);
     sim.fSimData.fSimulationSettings.simType = SPZAcousticData::ESimulationType::frequencyDomain;
-    sim.fSimData.fFourierSettings.nSamples = 50;
-    sim.fSimData.fFourierSettings.wMax = 2*M_PI*300;
+    sim.fSimData.fOutputSettings.resultsDir = "results/homogeneousFreq/";
+    sim.fSimData.fFourierSettings.nSamples = 150;
+    sim.fSimData.fFourierSettings.wMax = sim.fSimData.fSourceSettings.centralFrequency * 3;
     sim.fSimData.fFourierSettings.alphaFreqShift = -1;
 }
---
+++ Projects/AcousticsFourierAnalysis/createDirs.sh
@@ -0,0 +1,4 @@
+mkdir -p results/concentricTime
+mkdir -p results/concentricFreq
+mkdir -p results/homogeneousTime
+mkdir -p results/homogeneousFreq
